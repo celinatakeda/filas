3ebd3f32197ecd7569a7942daa16b0e1d71fe520
Celina Takeda <takedacarina@gmail.com>
2021-08-10 08:40:17 -0300

    commit

---
+++ fila/src/main/java/one/digitalinnovation/No.java
@@ -0,0 +1,41 @@
+package one.digitalinnovation;
+
+import java.util.Objects;
+
+public class No<T> {
+
+    private T object; //o object é o conteúdo do nó
+    private No<T> refNo;
+
+    public No(){
+
+    }
+
+    public No(T object) {
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "object=" + object +
+                '}';
+    }
+}
